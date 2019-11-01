969af8a0617ec4b702422639565170344c48c743
Benjamin Candy <bcandy93@gmail.com>
2019-11-01 01:04:32 -0700

    Added code to construct anchor tag and insert into the dropdown button (.dropdown-menu). Added ability to control how many results are returned within the ajax call.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,17 +6,38 @@ var appKey = "f76c23b37cb54db25ea370bc5b7a461f";
 
 
 
+function populateDropDown(objFood){
+
+    //clear existing drop-down items 
+    $(".dropdown-menu").empty()
+
+    //for each item returned in ajax call
+    for (var i = 1; i < objFood.hits.length; i++){
+        
+        //create anchor tag and apply applicable attributes
+        var newAnchor = $("<a>")
+        newAnchor.addClass("dropdown-item")
+        newAnchor.text(objFood.hits[i].fields.item_name)
+
+        //append newly created anchor tag to dropdown menu
+        $(".dropdown-menu").append(newAnchor)
+    }
+}
+
 
 
 $("#submit").on("click",function(){
+
+    //initialize variables
+    //foodSearch = the food string the user desires to search
     var foodSearch = $("#search").val()
-    console.log(foodSearch) 
+    //numOfResults = the number of items the user would like returned
+    var numOfResults = $("#numOfResults").val()
+    
     $.ajax({
-        url: "https://api.nutritionix.com/v1_1/search/" + foodSearch + "?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cnf_calories%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein%2Cnf_ingredient_statement&appId=5e176d12&appKey=f76c23b37cb54db25ea370bc5b7a461f",
+        url: "https://api.nutritionix.com/v1_1/search/" + foodSearch + "?results=0%3A" + numOfResults + "&cal_min=0&cal_max=50000&fields=item_name%2Cnf_calories%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein%2Cnf_ingredient_statement&appId=5e176d12&appKey=f76c23b37cb54db25ea370bc5b7a461f",
         method: "GET"
     }).then(function(response){
-        // console.log(response.hits[0]);
-        console.log("Item Name: " + response.hits[0].fields.item_name)
-        console.log("Calories: " + response.hits[0].fields.nf_calories)
+        populateDropDown(response)
     });
 })
