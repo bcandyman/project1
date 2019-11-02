1bff60b4c061f6e78af64fd1120d29c1fc7e99ba
Benjamin Candy <bcandy93@gmail.com>
2019-11-01 21:00:54 -0700

    Added program notes. Added ability to print information from ajax object to html document.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,7 @@
 // var searchTag = prompt("entery item: ");
 var appID = "5e176d12";
 var appKey = "f76c23b37cb54db25ea370bc5b7a461f";
+var nutritionCallResults = {}
 
 
 
@@ -12,11 +13,12 @@ function populateDropDown(objFood){
     $(".dropdown-menu").empty()
 
     //for each item returned in ajax call
-    for (var i = 1; i < objFood.hits.length; i++){
+    for (var i = 0; i < objFood.hits.length; i++){
         
         //create anchor tag and apply applicable attributes
         var newAnchor = $("<a>")
         newAnchor.addClass("dropdown-item")
+        newAnchor.attr("data-dropdownitem", i)
         newAnchor.text(objFood.hits[i].fields.item_name)
 
         //append newly created anchor tag to dropdown menu
@@ -26,7 +28,26 @@ function populateDropDown(objFood){
 
 
 
-$("#submit").on("click",function(){
+//This function will print a title and value to a target ID. 
+//The title or attribute and value will be separated by a ": "
+//Before printing, the value will be tested to ensure val contains information,
+//      if no information is held in val, nothing will be printed.
+function displayNutritionInformation(attr, val, targetID){
+
+    //convert val to string to enable testing on string length
+    var valString = val.toString().trim()
+
+    //if val contains any information
+    if (valString.length !== 0){
+        //print to id
+        $("#" + targetID).text(attr + ": " + val)
+    }
+}
+
+
+//Runs when getInformation button is clicked
+//Used when the user initiates a new food search
+$("#getInformation").on("click",function(){
 
     //initialize variables
     //foodSearch = the food string the user desires to search
@@ -38,6 +59,30 @@ $("#submit").on("click",function(){
         url: "https://api.nutritionix.com/v1_1/search/" + foodSearch + "?results=0%3A" + numOfResults + "&cal_min=0&cal_max=50000&fields=item_name%2Cnf_calories%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein%2Cnf_ingredient_statement&appId=5e176d12&appKey=f76c23b37cb54db25ea370bc5b7a461f",
         method: "GET"
     }).then(function(response){
-        populateDropDown(response)
+        console.log(response)
+        nutritionCallResults = response
+        populateDropDown(nutritionCallResults)
     });
+})
+
+
+$(document).on("click", ".dropdown-item", function(){
+
+    //retrieve data-dropdownitem value and set to the item user has clicked in the drop down
+    var itemClicked = $(this).attr("data-dropdownitem")
+    //Set the object item to objSelected
+    var objSelected = nutritionCallResults.hits[itemClicked].fields
+    // Set each variable to value of each attribute
+    var calories = objSelected.nf_calories
+    var fat = objSelected.nf_total_fat
+    var carbs = objSelected.nf_total_carbohydrate
+    var protein = objSelected.nf_protein
+    var ingredients = objSelected
+
+    //Pass information into fuction for printing on HTML
+    displayNutritionInformation("Calories", calories, "calories")
+    displayNutritionInformation("Fat", fat, "fat")
+    displayNutritionInformation("Carbohydrates", carbs, "carbs")
+    displayNutritionInformation("Protein", protein, "protein")
+    // displayNutritionInformation("Ingredients", ingredients, "ingredients")
 })
