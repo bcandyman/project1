c32725ff83bdb7f1e01e79f492e1d2244aeea264
Dianna Maynard <diannamaynard@me.com>
2019-11-03 09:23:58 -0800

    Added AJAX Query and HTML files
    Added HTML
    Added AJAX Query (I think)

---
+++ assets/js/jquery-ajax.js
@@ -0,0 +1,16 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+login(form) {
+    var un = form.Username.value;
+    var pw = form.Password.value;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("post", "Login", true);
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            loginResults();:exploding_head:
+Here is the event listener
+window.addEventListener(window,"load", function() {
+var loginForm = document.getElementById("LoginForm");
+window.addEventListener(loginForm, "submit", function() {
+     login(loginForm);
+ });
+});
