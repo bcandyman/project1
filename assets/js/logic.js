dfd45cf3da78f49ad0b1e5d5d5aeb6561b4da29f
Benjamin Candy <bcandy93@gmail.com>
2019-11-02 22:49:32 -0700

    Added ability to use inputs on side2 of card1 to search API for nutrition information. Added ability to select returned items and insert them into the top of the list for recording to firebase.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,84 +5,84 @@ var nutritionCallResults = {}
 
 
 
-
-
-function populateDropDown(objFood){
-
-    //clear existing drop-down items 
-    $(".dropdown-menu").empty()
+function populateFoodItems(objFood){
+    //clear items returned by search
+    $(".returnedFoodItems").find("*").not(".selected-item").empty()
 
     //for each item returned in ajax call
     for (var i = 0; i < objFood.hits.length; i++){
-        
-        //create anchor tag and apply applicable attributes
-        var newAnchor = $("<a>")
-        newAnchor.addClass("dropdown-item")
-        newAnchor.attr("data-dropdownitem", i)
-        newAnchor.text(objFood.hits[i].fields.item_name)
+    
+        //create div and apply applicable attributes
+        var newDiv = $("<div>")
+        newDiv.addClass("searched-item")
+        newDiv.attr("data-searchedFoodItem", i)
+        newDiv.text(objFood.hits[i].fields.item_name)
+        newDiv.append($("<hr>"))
 
         //append newly created anchor tag to dropdown menu
-        $(".dropdown-menu").append(newAnchor)
-    }
-}
-
-
-
-//This function will print a title and value to a target ID. 
-//The title or attribute and value will be separated by a ": "
-//Before printing, the value will be tested to ensure val contains information,
-//      if no information is held in val, nothing will be printed.
-function displayNutritionInformation(attr, val, targetID){
-
-    //convert val to string to enable testing on string length
-    var valString = val.toString().trim()
-
-    //if val contains any information
-    if (valString.length !== 0){
-        //print to id
-        $("#" + targetID).text(attr + ": " + val)
+        $(".returnedFoodItems").append(newDiv)
     }
 }
 
 
 //Runs when getInformation button is clicked
 //Used when the user initiates a new food search
-$("#getInformation").on("click",function(){
+$("#submit").on("click",function(){
 
     //initialize variables
     //foodSearch = the food string the user desires to search
-    var foodSearch = $("#search").val()
+    var foodSearch = $("#foodSearch").val()
     //numOfResults = the number of items the user would like returned
-    var numOfResults = $("#numOfResults").val()
+    var numOfResults = 5
+    console.log("foodSearch: " + foodSearch)
     
     $.ajax({
-        url: "https://api.nutritionix.com/v1_1/search/" + foodSearch + "?results=0%3A" + numOfResults + "&cal_min=0&cal_max=50000&fields=item_name%2Cnf_calories%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein%2Cnf_ingredient_statement&appId=5e176d12&appKey=f76c23b37cb54db25ea370bc5b7a461f",
+        url: "https://api.nutritionix.com/v1_1/search/" + foodSearch + "?results=0%3A" + numOfResults + "&cal_min=0&cal_max=50000&fields=*&appId=5e176d12&appKey=f76c23b37cb54db25ea370bc5b7a461f",
+        // url: "https://api.nutritionix.com/v1_1/search/" + foodSearch + "?results=0%3A" + numOfResults + "&cal_min=0&cal_max=50000&fields=item_name%2Cnf_calories%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein%2Cnf_ingredient_statement&appId=5e176d12&appKey=f76c23b37cb54db25ea370bc5b7a461f",
         method: "GET"
     }).then(function(response){
         console.log(response)
         nutritionCallResults = response
-        populateDropDown(nutritionCallResults)
+        populateFoodItems(nutritionCallResults)
     });
 })
 
 
-$(document).on("click", ".dropdown-item", function(){
+$(document).on("click", ".searched-item", function(){
 
-    //retrieve data-dropdownitem value and set to the item user has clicked in the drop down
-    var itemClicked = $(this).attr("data-dropdownitem")
+    //retrieve data-searchedfooditem value and set to the item user has clicked in the drop down
+    var itemClicked = $(this).attr("data-searchedfooditem")
+    console.log(itemClicked)
+
+     //Create div and apply applicable attributes
+     var newDiv = $("<div>")
+     newDiv.addClass("selected-item")
+     newDiv.text(nutritionCallResults.hits[itemClicked].fields.item_name)
+     newDiv.append($("<hr>"))
+
+     //append newly created div to returnedFoodItems class
+     $(".selectedFoodItems").prepend(newDiv)
+     $(".returnedFoodItems").empty()
+
     //Set the object item to objSelected
-    var objSelected = nutritionCallResults.hits[itemClicked].fields
+    // var objSelected = nutritionCallResults.hits[itemClicked].fields
     // Set each variable to value of each attribute
-    var calories = objSelected.nf_calories
-    var fat = objSelected.nf_total_fat
-    var carbs = objSelected.nf_total_carbohydrate
-    var protein = objSelected.nf_protein
-    var ingredients = objSelected
+    // var calories = objSelected.nf_calories
+    // var fat = objSelected.nf_total_fat
+    // var carbs = objSelected.nf_total_carbohydrate
+    // var protein = objSelected.nf_protein
+    // var ingredients = objSelected
 
     //Pass information into fuction for printing on HTML
-    displayNutritionInformation("Calories", calories, "calories")
-    displayNutritionInformation("Fat", fat, "fat")
-    displayNutritionInformation("Carbohydrates", carbs, "carbs")
-    displayNutritionInformation("Protein", protein, "protein")
+    // displayNutritionInformation("Calories", calories, "calories")
+    // displayNutritionInformation("Fat", fat, "fat")
+    // displayNutritionInformation("Carbohydrates", carbs, "carbs")
+    // displayNutritionInformation("Protein", protein, "protein")
     // displayNutritionInformation("Ingredients", ingredients, "ingredients")
+})
+
+$("#search").on("click",function(){
+    console.log("Hi")
+    $(".selectedFoodItems").empty()
+    $(".returnedFoodItems").empty()
 })
